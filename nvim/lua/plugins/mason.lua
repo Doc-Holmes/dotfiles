640f6e02920decbe6fec949e5baeeaaf9c682904
Manoel Tesar <tes210229@spengergasse.at>
2024-12-02 20:11:48 +0100

    Add LSP support and syntax highlighting

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+  "williamboman/mason.nvim",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "williamboman/mason-lspconfig",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    })
+    require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup_handlers {
+        -- The first entry (without a key) will be the default handler
+        -- and will be called for each installed server that doesn't have
+        -- a dedicated handler.
+        function (server_name) -- default handler (optional)
+            require("lspconfig")[server_name].setup {}
+        end
+    }
+  end,
+}
